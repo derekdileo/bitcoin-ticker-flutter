d8c6a61f1d3e9d99eb038fee019288996105b183
Derek DiLeo <derek@Dereks-Mac-mini.local>
2022-01-19 08:39:52 -0500

    Copy services/networking.dart from clima app and import http.dart to enable API calls.

--- lib/services/networking.dart
+++ lib/services/networking.dart
@@ -1,3 +1,22 @@
-import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 const String apiKey = '1C349635-A53A-4B2C-9A8A-0CF273FDE49C';
+
+class NetworkHelper {
+  final Uri url;
+
+  NetworkHelper(this.url);
+
+  Future getData() async {
+    http.Response response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+
+      return jsonDecode(data);
+    } else {
+      print(response.statusCode);
+    }
+  }
+}
